50d21f8f9c058a1f80c2b61c1a21669320f52c82
Andres <Andres@DESKTOP-00HTI69>
2025-08-26 20:53:17 -0300

    Mejoras del menu y tutorial(ahora son Jpanel)

--- src/Visual/Tutorial.java
+++ src/Visual/Tutorial.java
@@ -1,123 +1,97 @@
 package Visual;
 
 import java.awt.Color;
-import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class Tutorial {
+import GUI.Interfaz;
 
-	private JFrame frame;
+public class Tutorial extends JPanel {
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Tutorial window = new Tutorial();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private static final long serialVersionUID = 1L;
 
 	/**
-	 * Create the application.
+	 * Create the panel.
 	 */
 	public Tutorial() {
-		initialize();
-	}
-	public void setVisible(boolean visible) {
-		frame.setVisible(visible);
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 800, 640);
-		frame.getContentPane().setBackground(new Color(137, 108, 108));
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
+		setBackground(new Color(137, 108, 108));
+		setLayout(null);
 		JButton btnNewButton = new JButton("<=Atras");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				Interfaz.volverAlMenu();;
+			}
+		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 16));
-		btnNewButton.setBounds(552, 463, 173, 48);
-		frame.getContentPane().add(btnNewButton);
+		btnNewButton.setBounds(613, 511, 118, 38);
+		add(btnNewButton);
 		
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Interfaz interfaz = new Interfaz();
-				interfaz.setVisible(true);
-		}});
-
-		
-		JLabel lblNewLabel = new JLabel("New label");
+	
+		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setIcon(new ImageIcon("G:\\Nonograma\\ayuda.png"));
-		lblNewLabel.setBounds(30, 409, 352, 161);
-		frame.getContentPane().add(lblNewLabel);
+		lblNewLabel.setBounds(62, 448, 397, 135);
+		add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Idea  del juego ganado");
 		lblNewLabel_1.setForeground(new Color(0, 0, 0));
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblNewLabel_1.setBounds(30, 372, 286, 36);
-		frame.getContentPane().add(lblNewLabel_1);
+		lblNewLabel_1.setBounds(62, 402, 208, 22);
+		add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("El Objetivo es encontrar y marcar todas las casillas negras");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-		lblNewLabel_2.setBounds(10, 331, 540, 30);
-		frame.getContentPane().add(lblNewLabel_2);
+		lblNewLabel_2.setBounds(53, 353, 437, 19);
+		add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("– En las pistas, cada número indica una cadena de celdas negras consecutivas.");
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_3.setBounds(25, 47, 511, 30);
-		frame.getContentPane().add(lblNewLabel_3);
+		lblNewLabel_3.setBounds(48, 72, 511, 16);
+		add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("- Entre dos cadenas de celdas negras debe existir al menos una celda libre, en el ejemplo");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_4.setBounds(25, 88, 585, 36);
-		frame.getContentPane().add(lblNewLabel_4);
+		lblNewLabel_4.setBounds(53, 119, 576, 16);
+		add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("las marcadas con X.");
 		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_5.setBounds(25, 119, 143, 24);
-		frame.getContentPane().add(lblNewLabel_5);
+		lblNewLabel_5.setBounds(63, 147, 129, 16);
+		add(lblNewLabel_5);
 		
 		JLabel lblNewLabel_6 = new JLabel("");
 		lblNewLabel_6.setIcon(new ImageIcon("G:\\Nonograma\\ayuda2.png"));
-		lblNewLabel_6.setBounds(552, 47, 143, 36);
-		frame.getContentPane().add(lblNewLabel_6);
+		lblNewLabel_6.setBounds(595, 68, 132, 20);
+		add(lblNewLabel_6);
 		
 		JLabel lblNewLabel_7 = new JLabel("-Al costado de cada fila en la grilla, aparecen los largos de las cadenas de casillas en");
 		lblNewLabel_7.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_7.setBounds(25, 161, 561, 36);
-		frame.getContentPane().add(lblNewLabel_7);
+		lblNewLabel_7.setBounds(53, 187, 548, 16);
+		add(lblNewLabel_7);
 		
 		JLabel lblNewLabel_8 = new JLabel("negro para esa fila.");
 		lblNewLabel_8.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_8.setBounds(25, 192, 242, 30);
-		frame.getContentPane().add(lblNewLabel_8);
+		lblNewLabel_8.setBounds(62, 214, 126, 16);
+		add(lblNewLabel_8);
 		
 		JLabel lblNewLabel_9 = new JLabel("– Sobre cada columna en la grilla, aparecen los largos de las cadenas de casillas en negro");
 		lblNewLabel_9.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_9.setBounds(25, 233, 595, 30);
-		frame.getContentPane().add(lblNewLabel_9);
+		lblNewLabel_9.setBounds(53, 256, 579, 16);
+		add(lblNewLabel_9);
 		
 		JLabel lblNewLabel_10 = new JLabel("para esa columna.");
 		lblNewLabel_10.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel_10.setBounds(25, 258, 233, 24);
-		frame.getContentPane().add(lblNewLabel_10);
+		lblNewLabel_10.setBounds(62, 283, 118, 16);
+		add(lblNewLabel_10);
 	}
 
 }
